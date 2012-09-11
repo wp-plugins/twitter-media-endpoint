4e01a8f869936fb49dc00f08b250d582fd2a4912
sterling <sterling@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-11 17:22:39 +0000

    version 0.8
    git-svn-id: https://plugins.svn.wordpress.org/twitter-media-endpoint/trunk@597606 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twittermedia.php
+++ twittermedia.php
@@ -3,7 +3,7 @@
 Plugin Name: Twitter Media Endpoint
 Plugin URI: http://sterlinganderson.net/twitter-media-endpoint
 Description: Allow your WP install to be a Twitter Media Endpoint
-Version: 0.7
+Version: 0.8
 Author: Sterling Anderson
 Author URI: http://sterlinganderson.net
 License: MIT
